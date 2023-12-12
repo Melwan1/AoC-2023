87e5beb461e5196727e64d3b2c003193aff6d149
Melwan Chevassus <melwan.chevassus@epita.fr>
2023-12-12 17:53:25 +0100

    solution(day_02): add cube_conundrum_2.c

---
+++ day_02/cube_conundrum_2.c
@@ -0,0 +1,113 @@
+#include <err.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <fnmatch.h>
+
+struct box
+{
+    int blue;
+    int red;
+    int green;
+};
+
+void next_item(char **buffer)
+{
+    while(**buffer != ',' && **buffer != ';' && **buffer)
+    {
+        (*buffer)++;
+    }
+    if (**buffer == ',')
+    {
+        *buffer += 2;
+    }
+}
+
+int read_result(char *buffer, struct box *result_box)
+{
+    if (!(*buffer))
+    {
+        return 0;
+    }
+    for (int index = 0; index < 3; index++)
+    {
+        int quantity = 0;
+        if (sscanf(buffer, "%d", &quantity))
+        {
+            while (*buffer != ' ')
+            {
+                buffer++;
+            }
+            buffer++;
+        }
+        if (!fnmatch("red*", buffer, 0))
+        {
+            if (quantity > result_box->red)
+            {
+                result_box->red = quantity;
+            }
+            next_item(&buffer);
+        }
+
+        else if (!fnmatch("blue*", buffer, 0))
+        {
+            if (quantity > result_box->blue)
+            {
+            result_box->blue = quantity;
+            }
+            next_item(&buffer);
+        }
+
+        else if (!fnmatch("green*", buffer, 0))
+        {
+            if (quantity > result_box->green)
+            {
+                result_box->green = quantity;
+            }
+            next_item(&buffer);
+        }
+    }
+    return 1;
+}
+
+int read_game(char *buffer)
+{
+    int game_id = 0;
+    sscanf(buffer, "Game %d", &game_id);
+    while (*buffer != ':')
+    {
+        buffer++;
+    }
+    buffer += 2;
+    struct box reference = { 0, 0, 0};
+    result = read_result(buffer, &reference);
+    while (result == 1)
+    {
+        while (*buffer && *buffer != ';')
+        {
+            buffer++;
+        }
+        if (*buffer == ';')
+        {
+            buffer += 2;
+        }
+        result = read_result(buffer, &reference);
+    }
+    return reference.red * reference.blue * reference.green;
+}
+
+int main(void)
+{
+    FILE * file = fopen("input.txt", "r");
+    if (!file)
+    {
+        errx(1, "file could not be opened");
+    }
+    char buffer[256];
+    int result = 0;
+    while (fgets(buffer, 256, file))
+    {
+        result += read_game(buffer);
+    }
+    printf("%d\n", result);
+    return 0;
+}
